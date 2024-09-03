9a502f35bc3065e0a7c487f034ff889ab8236dd4
Volodymyr Antoniuk <voxtobox@gmail.com>
2024-09-03 11:21:33 -0700

    add

--- js/task-3.js
+++ js/task-3.js
@@ -12,3 +12,11 @@ const profile = {
     return `${this.username} has ${this.playTime} active hours!`
   },
 };
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
